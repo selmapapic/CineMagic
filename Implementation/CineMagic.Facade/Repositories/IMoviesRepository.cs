3c659ce7480418306b74ec11eeba708d85518f34
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-05-21 02:12:47 +0200

    Dodan folder sa interfejsom u .Facade

---
+++ Implementation/CineMagic.Facade/Repositories/IMoviesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CineMagic.Facade.Repositories
+{
+    public interface IMoviesRepository
+    {
+        Task<MovieGetDetailsRes> GetDetailsAsync(MovieGetDetailsReq req);
+        Task<IList<MovieGetDetailsRes>> GetAllMoviesAsync();
+    }
+}
