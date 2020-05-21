5a6fc24f0fda045c1f2767da63065e549edcfc14
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-05-21 02:07:50 +0200

    Dodan dbContext

---
+++ Implementation/CineMagic.Dal/Context/CineMagicDbContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CineMagic.Dal.Context
+{
+    public class CinemaResDbContextDesignTimeFactory : IDesignTimeDbContextFactory<CineMagicDbContext>
+    {
+        public CineMagicDbContext CreateDbContext(string[] args)
+        {
+            ConfigurationBuilder cfgBuilder = new ConfigurationBuilder();
+
+            cfgBuilder.AddJsonFile("appSettings.Development.json", true);
+
+            IConfigurationRoot configuration = cfgBuilder.Build();
+
+            string connectionString = configuration.GetConnectionString(typeof(CineMagicDbContext).Name);
+
+            DbContextOptionsBuilder<CineMagicDbContext> optionsBuilder = new DbContextOptionsBuilder<CineMagicDbContext>();
+
+            optionsBuilder.UseSqlServer(connectionString, sqlServerOptions =>
+            {
+                sqlServerOptions.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            optionsBuilder.EnableSensitiveDataLogging();
+
+            return new CineMagicDbContext(optionsBuilder.Options);
+        }
+    }
+}
