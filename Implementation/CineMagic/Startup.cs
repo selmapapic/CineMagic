a8f09bf107769a4ef2ec3a6b82a2a2f3d9b35ef0
kdokic1 <kdokic1@etf.unsa.ba>
2020-05-21 03:21:28 +0200

    ispravljena metoda ConfigureServices u startup.cs

--- Implementation/CineMagic/Startup.cs
+++ Implementation/CineMagic/Startup.cs
@@ -30,11 +30,11 @@ namespace CineMagic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<CineMagicDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("CineMagicDbContext")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<CineMagicDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
 
